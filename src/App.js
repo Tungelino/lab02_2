ad608e48f1faadc2c231942266a2caa9d50b42e1
TungNguyen <tungnguyen.neu@gmail.com>
2021-10-21 13:34:50 +0900

    Component Part2__Lab03-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+
   render() {
     return (
       <div className='App'>
@@ -14,7 +25,7 @@ class App extends Component {
 
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
 
       </div>
     )
